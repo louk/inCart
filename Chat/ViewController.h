ed295f880f7da98ee244844398939fc6931eab81
KevinPy <nova313@hotmail.fr>
2014-11-12 22:51:33 +0100

    Initial Commit

---
+++ Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat
+//
+//  Created by Kevin on 12/11/2014.
+//  Copyright (c) 2014 YOUR_ORGANIZATION_NAME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Chat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Chat
+//
+//  Created by Kevin on 12/11/2014.
+//  Copyright (c) 2014 YOUR_ORGANIZATION_NAME. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
